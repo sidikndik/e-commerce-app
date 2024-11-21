d118a65f1a49168fa26d74c3ab7333056d214df6
sidikndik <sidikndik92@gmail.com>
2024-11-22 06:18:47 +0700

    add data seed category

--- model/category.go
+++ model/category.go
@@ -4,3 +4,19 @@ type Category struct {
 	Base
 	Name string `json:"name" gorm:"type:varchar(100);not null"`
 }
+
+func CategorySeed() []Category {
+	categories := []Category{
+		{Name: "Elektronik"},
+		{Name: "Peralatan Rumah Tangga"},
+		{Name: "Fashion Pria"},
+		{Name: "Fashion Wanita"},
+		{Name: "Kesehatan dan Kecantikan"},
+		{Name: "Makanan dan Minuman"},
+		{Name: "Olahraga dan Outdoor"},
+		{Name: "Mainan dan Hobi"},
+		{Name: "Buku dan Alat Tulis"},
+		{Name: "Otomotif"},
+	}
+	return categories
+}
